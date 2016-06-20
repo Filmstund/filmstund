3dbf6ea8967c36e47f060fafba44a7ca4427f796
André Samuelsson <andresamuelsson94@gmail.com>
2016-06-20 21:58:04 +0200

    linting

--- frontend/seed/index.js
+++ frontend/seed/index.js
@@ -1,18 +1,18 @@
 const r = require('rethinkdb');
 
-const bioord = require('./bioord.json')
-const movies = require('./movies.json')
+const bioord = require('./bioord.json');
+const movies = require('./movies.json');
 
 r.connect({ host: 'localhost', port: 63164 }, (err, conn) => {
-  if(err) throw err;
+  if(err) {throw err;}
 
 
   const createTables = () => {
     return r.db('itbio').tableCreate('movies').run(conn)
       .then(() => r.db('itbio').table('movies').insert(movies).run(conn))
       .then(() => r.db('itbio').tableCreate('bioord').run(conn))
-      .then(() => r.db('itbio').table('bioord').insert(bioord).run(conn))
-  }
+      .then(() => r.db('itbio').table('bioord').insert(bioord).run(conn));
+  };
 
   Promise.all([
     r.db('itbio').tableDrop('movies').run(conn),
@@ -21,7 +21,7 @@ r.connect({ host: 'localhost', port: 63164 }, (err, conn) => {
   .catch(() => {})
   .then(createTables)
   .catch(console.error)
-  .then(() => conn.close())
+  .then(() => conn.close());
 
 
 });
