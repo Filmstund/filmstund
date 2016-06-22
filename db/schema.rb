3ab04524c43b5b4a2dcfedf32d3ca9be1c76c542
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-06-22 20:02:50 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621142915) do
+ActiveRecord::Schema.define(version: 20160622162605) do
 
   create_table "api_tokens", force: :cascade do |t|
     t.integer  "user_id"
