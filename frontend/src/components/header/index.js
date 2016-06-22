b0e37b18343e94e3dae745e66a82b62204ef31d3
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-06-22 14:36:05 +0200

    Frontend token authentication

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -7,12 +7,25 @@ import Quotationsbar from './quotationsbar';
 import Navbar from './navbar';
 import styles from './style.css';
 
+const fetchWithToken = (url) => {
+  const token = localStorage.getItem('token')
+
+  return fetch(url, {
+    headers: {
+      'Authorization': `Token token="${token}"`
+    }
+  }).then(d => d.json())
+}
+
 const Header = React.createClass({
   handleGoogleAuthCallback(params) {
-
+    return this.handleAuthCallback({
+      token: params.hg.id_token,
+      user_id: params.wc.Ka,
+      provider: 'google'
+    })
   },
   handleFacebookAuthCallback(params) {
-    console.log(params);
     return this.handleAuthCallback({
       token: params.accessToken,
       user_id: params.userID,
@@ -20,8 +33,6 @@ const Header = React.createClass({
     })
   },
   handleAuthCallback(params) {
-    console.log(params);
-
     if (!params.token || !params.user_id) {
       throw 'Authentication failure - no token received'
     }
@@ -34,22 +45,56 @@ const Header = React.createClass({
       method: 'post',
       body: postBody
     })
+    .then(d => d.json())
+    .then(json => {
+      console.log(json.token);
+      localStorage.setItem('token', json.token)
+      this.forceUpdate()
+
+      fetch('/api/me', {
+        headers: {
+          'Authorization': `Token token="${json.token}"`
+        }
+      })
+    })
+  },
+  signOut() {
+    fetchWithToken('/api/signout').then(() => {
+      localStorage.removeItem('token')
+      this.forceUpdate()
+    })
+  },
+  fetchMe() {
+    fetchWithToken('/api/me')
   },
   render() {
+    let signedIn = false;
+    if (localStorage.getItem('token')) {
+      signedIn = true;
+    }
+
     return (
       <div className={styles.container}>
         <Navbar/>
         <Quotationsbar />
+        {!signedIn &&
         <GoogleLogin
           clientId="692064172675-montab9pi57304e68r932c6lm7111oaf.apps.googleusercontent.com"
           scope="profile email"
           callback={this.handleGoogleAuthCallback}
-        />
+        />}
+        {!signedIn &&
         <FacebookLogin
           appId="241451712906812"
-          scope="email,public_profile"
+          scope="email"
           callback={this.handleFacebookAuthCallback}
-        />
+        />}
+        {signedIn &&
+          <button onClick={this.signOut}>Sign out</button>
+        }
+        {signedIn &&
+          <button onClick={this.fetchMe}>Fetch me</button>
+        }
       </div>
     )
   }
