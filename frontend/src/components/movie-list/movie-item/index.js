063b1223a218b1f8cd510c7efb82db7d356270be
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-06-22 23:30:32 +0200

    show premiere_date for movie items

--- frontend/src/components/movie-list/movie-item/index.js
+++ frontend/src/components/movie-list/movie-item/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import moment from 'moment';
+
+moment.locale('sv')
 
 import styles from './style.css';
 
@@ -11,12 +14,17 @@ const MovieItem = React.createClass({
         <div className={styles.image} style={{backgroundImage: `url(${movie.poster})`}}></div>
         <div className={styles.description}>
           <div className={styles.header}>
+            <div className={styles.leftSide}>
               {movie.imdb_id &&
                 <a href={`http://www.imdb.com/title/${movie.imdb_id}/`} className={styles.imdbLogo}>
                   <img src='/IMDb_logo.svg' />
                 </a>
               }
-            <h3> {movie.title} </h3>
+              <h3>{movie.title}</h3>
+            </div>
+            <div>
+              <time dateTime={moment(movie.premiere_date).toISOString()} title={moment(movie.premiere_date).format('L')}>Premiär {moment(movie.premiere_date).fromNow()}</time>
+            </div>
           </div>
           {movie.description}
           <div className={styles.footer}>wey</div>
