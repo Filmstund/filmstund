bff411b70db3abb47c31df2b26493d1b4d3d3db2
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-06-22 14:32:53 +0200

    ApiToken model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622162605) do
+ActiveRecord::Schema.define(version: 20160621142915) do
+
+  create_table "api_tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_api_tokens_on_user_id"
+  end
 
   create_table "attendees", force: :cascade do |t|
     t.integer  "user_id"
