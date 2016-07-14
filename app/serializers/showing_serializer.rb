fcceb4daaa09c432ffa4086e7c923705aec795f9
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-07-15 00:32:38 +0200

    show owner and is_admin in serializer

--- app/serializers/showing_serializer.rb
+++ app/serializers/showing_serializer.rb
@@ -1,5 +1,10 @@
 class ShowingSerializer < ActiveModel::Serializer
-  attributes :id, :status, :created_at, :updated_at
+  attributes :id, :status, :created_at, :updated_at, :is_admin
   belongs_to :movie
+  belongs_to :owner
   has_many :time_slots
+
+  def is_admin
+    object.owner == current_user
+  end
 end
