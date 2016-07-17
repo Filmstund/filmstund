b47b6456e5f59b1dfe327de8431387ce0f8882ed
juice <juice@chalmers.it>
2016-07-17 22:39:38 +0200

    Fance showing list

--- frontend/src/components/showing-list/showing-item/index.js
+++ frontend/src/components/showing-list/showing-item/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import loader from '../../loader';
 
 import StatusLabel from '../../status-label';
+import MovieInfo from '../../movie-info';
 
 import styles from './style.css';
 
@@ -11,11 +12,14 @@ const ShowingItem = React.createClass({
 
     return (
       <div className={styles.container}>
-        <div className={styles.image} style={{backgroundImage: `url(${showing.movie.poster})`}}></div>
-        <div className={styles.description}>
-          <a href={`/showings/${showing.id}`} ><h3>{showing.movie.title}</h3></a>
-          <StatusLabel className={styles.label} status={showing.status} />
+        <div className={styles.header}>
+          <a href={`/showings/${showing.id}`} ><h2>{showing.movie.title}</h2></a>
+          <div className={styles.statusLabel}><StatusLabel className={styles.label} status={showing.status} /></div>
         </div>
+        <div className={styles.showingInfo}>
+          Admin: {showing.owner.nick}
+        </div>
+        <MovieInfo movie={showing.movie} />
       </div>
     )
   }
