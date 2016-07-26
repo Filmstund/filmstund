5d73b95e54db7a0eed28c3cc450c55070ee2c3da
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-07-27 00:46:41 +0200

    move push file and set mailer defaults for route-helpers

---
+++ lib/push.rb
@@ -0,0 +1,30 @@
+class Push
+  class << self
+    include Rails.application.routes.url_helpers
+
+    def showing_created(showing)
+      @showing = showing
+
+      User.with_push_key.each do |user|
+        Pushover.notification(message: @showing.movie.title,
+                              user: user.pushover_key,
+                              title: "#{@showing.owner.nick} created showing!",
+                              url: showing_url(@showing),
+                              url_title: "Link to showing")
+      end
+    end
+
+    def showing_confirmed(showing)
+      @showing = showing
+      @time_slot = @showing.selected_time_slot
+
+      User.with_push_key.each do |user|
+        Pushover.notification(message: @showing.movie.title,
+                              user: user.pushover_key,
+                              title: "TimeSlot was chosen for Showing!",
+                              url: showing_url(@showing),
+                              url_title: "Link to showing")
+      end
+    end
+  end
+end
