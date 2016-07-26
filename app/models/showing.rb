806076b0653276b33a8261b53065181d31174878
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-07-26 20:56:09 +0200

    status is now enum

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -1,4 +1,5 @@
 class Showing < ApplicationRecord
+  enum status: [ :cancelled, :open, :confirmed, :done ]
   belongs_to :owner, class_name: User
   belongs_to :movie, foreign_key: :sf_id
   has_many :time_slots
