ea2f08c0bb290910b82c379ad1d8a398009bc2f8
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-10-08 00:16:49 +0200

    validations and relations from GiftCard

--- app/models/gift_card.rb
+++ app/models/gift_card.rb
@@ -1,2 +1,5 @@
 class GiftCard < ApplicationRecord
+  belongs_to :owner, class_name: User
+  enum type: [ :bioklubbskort, :foretagsbiljett, :rabattkort, :presentkort ]
+  validates :number, format: {with: /\A(\d{8}|\d{11})\z/, message: 'Invalid number id'}
 end
