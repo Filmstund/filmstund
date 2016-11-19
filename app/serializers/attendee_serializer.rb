20b677716fb427eee972f57f865cdd792b2d8313
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-11-19 13:59:09 +0100

    Extract lambda into method

--- app/serializers/attendee_serializer.rb
+++ app/serializers/attendee_serializer.rb
@@ -1,8 +1,12 @@
 class AttendeeSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :nick
-  attribute :gift_card, if: -> { object.showing.owner == current_user || object.user == current_user }
+  attribute :gift_card, if: :user_owns_showing_or_card
 
   def nick
     object.user.nick
+  end
+
+  def user_owns_showing_or_card
+    object.showing.owner == current_user || object.user == current_user
   end
 end
