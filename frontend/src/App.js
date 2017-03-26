ec2c4d4c4e2ffbebdb09f605e9fc525c6ef98de9
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-03-26 17:06:54 +0200

    add padding box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import styled from "styled-components";
 
 import TopBar from "./TopBar";
 import Container from "./Container";
@@ -7,15 +8,21 @@ import Home from "./routes/Home";
 import Showings from "./routes/Showings";
 import User from "./routes/User";
 
+const PaddingContainer = styled.div`
+  padding: 1em;
+`;
+
 const App = React.createClass({
   render() {
     return (
       <Router>
         <Container>
           <TopBar/>
-          <Route exact path="/" component={Home} />
-          <Route path="/showings" component={Showings} />
-          <Route path="/user" component={User} />
+          <PaddingContainer>
+            <Route exact path="/" component={Home} />
+            <Route path="/showings" component={Showings} />
+            <Route path="/user" component={User} />
+          </PaddingContainer>
         </Container>
       </Router>
     );
