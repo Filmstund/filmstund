787108d7c8f007a616c9ecb947d55d0afee386dd
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-03-26 22:01:34 +0200

    add fetch and get/post methods

---
+++ frontend/src/lib/rest.js
@@ -0,0 +1,24 @@
+import "whatwg-fetch";
+
+const backendUrl = "http://localhost:8080";
+
+const checkStatusAndParseJson = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return response.json();
+    } else {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+};
+
+export const getJson = (path) =>
+    fetch(backendUrl + path)
+    .then(checkStatusAndParseJson);
+
+export const postJson = (path, data) =>
+    fetch(backendUrl + path, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+    .then(checkStatusAndParseJson);
