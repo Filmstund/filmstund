adfd181ce86c065d03bc844f44721d325046e2ce
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-03-26 22:01:02 +0200

    add cors header

--- backend/src/main/kotlin/rocks/didit/sefilm/Application.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/Application.kt
@@ -5,16 +5,30 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import rocks.didit.sefilm.repositories.LocationRepository
 import rocks.didit.sefilm.repositories.MovieRepository
 import rocks.didit.sefilm.repositories.ShowingRepository
 import rocks.didit.sefilm.repositories.UserRepository
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+
 
 @SpringBootApplication
 class Application {
     private val log = LoggerFactory.getLogger(Application::class.java)
     @Bean
     fun init(showings: ShowingRepository, movies: MovieRepository, locations: LocationRepository, users: UserRepository) = CommandLineRunner {
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry?) {
+                registry!!.addMapping("/**").allowedOrigins("*")
+            }
+        }
     }
 }
 
