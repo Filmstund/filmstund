a065fa8ef3b406e1b051e4726c7606852237a1f0
Emil Edholm <bobby@verypublic.org>
2017-03-26 21:37:33 +0200

    Don't return email and bioklubbnummer

--- backend/src/main/kotlin/rocks/didit/sefilm/database/entities/User.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/database/entities/User.kt
@@ -13,7 +13,9 @@ data class User(
         val id: UUID = UUID.randomUUID(),
         val name: String,
         val nick: String?,
+        @JsonIgnore
         val email: String,
+        @JsonIgnore
         val bioklubbnummer: Bioklubbnummer,
         val phone: String,
         @JsonIgnore
