61e545b5b7a3ae52f1f5200245e61bb7f0ef97a4
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-03-26 19:20:01 +0200

    main button component

---
+++ frontend/src/MainButton.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const MainButton = styled.button`
+  background-color: gold;
+  color: black;
+  border: 0;
+  height: 3em;
+  margin: 1em 0;
+  font-size: 1.1em;
+  font-weight: 700;
+  width: 100%;
+  font-family: "Roboto", sans-serif;
+`;
+
+export default MainButton
--- frontend/src/routes/Showings.js
+++ frontend/src/routes/Showings.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import MainButton from "../MainButton";
 
 const Showings = ({ className }) => (
   <div className={className}>
     this is showings
+      <MainButton>This is main action</MainButton>
   </div>
 )
 
