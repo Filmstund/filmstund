e0b702cf2258c04130858107dddb23753c5490af
Emil Edholm <bobby@verypublic.org>
2017-03-28 16:42:52 +0200

    Changed date/time type

--- backend/src/main/kotlin/rocks/didit/sefilm/database/entities/Showing.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/database/entities/Showing.kt
@@ -2,13 +2,15 @@ package rocks.didit.sefilm.database.entities
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
 import java.util.*
 
 @Document
 data class Showing(@Id
                    val id: UUID = UUID.randomUUID(),
-                   val startTime: Instant? = null,
+                   val date: LocalDate? = null,
+                   val time: LocalTime? = null,
                    val movie: Movie? = null,
                    val location: Location? = null,
                    val admin: User? = null,
