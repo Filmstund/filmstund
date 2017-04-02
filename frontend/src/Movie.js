44366f2970a245b40d04da70876ebdd011ee11bd
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-04-02 13:16:24 +0200

    show premiere when not premeiered yet

--- frontend/src/Movie.js
+++ frontend/src/Movie.js
@@ -1,18 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import {formatShowingDate} from "./lib/dateTools";
+import {formatShowingDate, getTodaysDate, showingDateToString} from "./lib/dateTools";
 import PosterBox from "./PosterBox";
 
 const VerticalPaddingContainer = styled.div`
   padding: 1em 0;
 `;
 
+const now = getTodaysDate();
+
+const renderPremiereDate = (releaseDate) => {
+
+    const showingDate = showingDateToString(releaseDate);
+    const formattedDate = formatShowingDate(releaseDate);
+
+    if (showingDate > now) {
+        return "Premiär " + formattedDate;
+    } else {
+        return null;
+    }
+};
+
+
 
 const Movie = ({ className, movie: { poster, title, releaseDate } }) => (
     <div className={className}>
         <PosterBox headerText={title} poster={poster}>
             <VerticalPaddingContainer>
-                Premiär {formatShowingDate(releaseDate)}
+                {renderPremiereDate(releaseDate)}
             </VerticalPaddingContainer>
         </PosterBox>
     </div>
