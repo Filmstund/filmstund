332063b834e690891a05e4fc66b12659aee97154
Emil Edholm <bobby@verypublic.org>
2017-04-15 22:10:46 +0200

    Remove redunt JsonFormat

--- backend/src/main/kotlin/rocks/didit/sefilm/database/entities/Showing.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/database/entities/Showing.kt
@@ -1,6 +1,5 @@
 package rocks.didit.sefilm.database.entities
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.mongodb.core.mapping.Document
@@ -12,9 +11,7 @@ import java.util.*
 @Document
 data class Showing(@Id
                    val id: UUID = UUID.randomUUID(),
-                   @JsonFormat(pattern = "yyyy-MM-dd")
                    val date: LocalDate? = null,
-                   @JsonFormat(pattern = "HH:mm")
                    val time: LocalTime? = null,
                    val movieId: UUID? = null,
                    val location: Location? = null,
