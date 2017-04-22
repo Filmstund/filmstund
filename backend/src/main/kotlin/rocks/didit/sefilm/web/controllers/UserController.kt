2ebdcbf5dc8e900fa3e86d7a7c62ede750aa0696
Emil Edholm <bobby@verypublic.org>
2017-04-22 21:55:49 +0200

    Make use of extension function

--- backend/src/main/kotlin/rocks/didit/sefilm/web/controllers/UserController.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/web/controllers/UserController.kt
@@ -1,13 +1,12 @@
 package rocks.didit.sefilm.web.controllers
 
 import org.springframework.http.MediaType
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import rocks.didit.sefilm.Application
 import rocks.didit.sefilm.NotFoundException
-import rocks.didit.sefilm.OpenIdConnectUserDetails
+import rocks.didit.sefilm.currentLoggedInUserId
 import rocks.didit.sefilm.database.entities.User
 import rocks.didit.sefilm.database.repositories.UserRepository
 import rocks.didit.sefilm.domain.LimitedUserInfo
@@ -21,8 +20,10 @@ class UserController(val userRepository: UserRepository) {
 
     @GetMapping(BASE_PATH + "/me", produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
     fun currentUser(): User {
-        val principal = SecurityContextHolder.getContext().authentication.principal as OpenIdConnectUserDetails
-        return userRepository.findOne(principal.userId).orElseThrow { NotFoundException("user '${principal.userId}'") }
+        val currentLoggedInUserId = currentLoggedInUserId()
+        return userRepository
+                .findOne(currentLoggedInUserId)
+                .orElseThrow { NotFoundException("user '$currentLoggedInUserId'") }
     }
 
     @GetMapping(BASE_PATH, produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
