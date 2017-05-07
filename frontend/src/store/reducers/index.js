801ae8e0a8f75426ecd579f0121356f039cd0963
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-05-07 14:31:48 +0200

    me reducer separated from users reducer

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -3,6 +3,7 @@ import _ from "lodash";
 
 import createCrudReducer, {crudSingleReducer} from "./rest";
 import {withBaseURL} from "../../lib/fetch";
+import meReducer from './me'
 
 const showingTransformer = (showing) => ({
     ..._.omit(showing, 'time'),
@@ -11,12 +12,14 @@ const showingTransformer = (showing) => ({
 
 export const showings = createCrudReducer("SHOWINGS", withBaseURL("/showings"), showingTransformer);
 export const movies = createCrudReducer("MOVIES", withBaseURL("/movies"));
-export const me = crudSingleReducer("ME", withBaseURL("/users/me"));
+export const users = createCrudReducer("USERS", withBaseURL("/users"));
 export const bioord = crudSingleReducer("BIOORD", withBaseURL("/budord/random"));
+export const me = meReducer;
 
 export default combineReducers({
     me: me.reducer,
     showings: showings.reducer,
+    users: users.reducer,
     movies: movies.reducer,
     bioord: bioord.reducer
 });
---
+++ frontend/src/store/reducers/me.js
@@ -0,0 +1,86 @@
+import _ from "lodash";
+import fetch, { jsonRequest } from "../../lib/fetch";
+import {withBaseURL} from "../../lib/fetch";
+import {USER_SIGNOUT_ACTION} from "./user";
+
+const path = withBaseURL("/users/me")
+
+const actions = {
+    requestSingle: `ME_REQUEST_SINGLE`,
+    successSingle: `ME_RESPONSE_SINGLE`,
+    errorSingle: `ME_ERROR_SINGLE`,
+
+    requestUpdate: `ME_REQUEST_UPDATE`,
+    successUpdate: `ME_RESPONSE_UPDATE`,
+    errorUpdate: `ME_ERROR_UPDATE`,
+
+    clearSingle: `ME_CLEAR_SINGLE`,
+};
+
+const initialState = {
+    loading: false,
+    data: {},
+    error: null
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actions.requestSingle:
+        case actions.requestUpdate:
+            return {
+                ...state,
+                error: null,
+                loading: true
+            };
+        case actions.successSingle:
+        case actions.successUpdate:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                data: action.data
+            };
+        case actions.errorSingle:
+        case actions.errorUpdate:
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            };
+
+        case USER_SIGNOUT_ACTION:
+        case actions.clearSingle:
+            return initialState;
+        default:
+            return state;
+    }
+};
+
+const actionCreators = {
+
+    requestSingle: () => (dispatch) => {
+        dispatch({ type: actions.requestSingle });
+
+        fetch(path)
+            .then(data => {
+              dispatch({ type: actions.successSingle, data })
+              dispatch({ type: 'USERS_RESPONSE_SINGLE', data })
+            })
+            .catch(error => dispatch({ type: actions.errorSingle, error }))
+    },
+
+    requestUpdate: (data) => (dispatch) => {
+        dispatch({ type: actions.requestUpdate, data });
+
+        jsonRequest(path, data, "PUT")
+            .then(data => dispatch({ type: actions.successUpdate, data }))
+            .catch(error => dispatch({ type: actions.errorUpdate, error }))
+    },
+
+    clearSingle: () => ({ type: actions.clearSingle })
+};
+
+export default {
+    reducer,
+    actions: actionCreators
+};
