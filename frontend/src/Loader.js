9c5efdd73c2053665fd1bd3e28329b9b80d9312c
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-05-08 18:58:40 +0200

    Loader component

---
+++ frontend/src/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loader = ({ color = '#000', size = 150 }) => (
+  <svg width={size + 'px'} height={size + 'px'} viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke={color}><rect fill="#ffffff" width="100%" height="100%" strokeWidth="0"></rect>
+    <g fill="none" fillRule="evenodd" strokeWidth="2">
+        <circle cx="22" cy="22" r="1">
+            <animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"></animate>
+            <animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"></animate>
+        </circle>
+        <circle cx="22" cy="22" r="1">
+            <animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"></animate>
+            <animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"></animate>
+        </circle>
+    </g>
+  </svg>
+)
+
+
+export default Loader
