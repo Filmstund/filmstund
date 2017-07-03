9cc7b2a2bb3816c6275c926a419b054a5fd7ab2e
Johan Horvius <horv@chalmers.it>
2017-07-03 20:48:57 +0200

    Added search bar for creating new showings

--- frontend/src/routes/NewShowing.js
+++ frontend/src/routes/NewShowing.js
@@ -12,12 +12,17 @@ import Loader from "../ProjectorLoader";
 import Movie from "../Movie";
 import CreateShowingForm from "../CreateShowingForm";
 
+import Field from "../Field";
+import Input from "../Input";
+
 class NewShowing extends React.Component {
   constructor(props) {
     super(props);
     const { match: { params: { movieId } } } = props;
 
-    this.state = { movieId };
+    this.state = { movieId,
+      searchTerm: ""
+    };
   }
 
   componentWillMount() {
@@ -41,17 +46,50 @@ class NewShowing extends React.Component {
     }
   };
 
+  setSearchTerm(term) {
+    let value = term.target.value
+    if(value) {
+      value = value.toLowerCase()
+    }
+    this.setState({
+      searchTerm: value
+    })
+  }
+
+  searchFilter(m) {
+
+    const { searchTerm } = this.state;
+
+    if(searchTerm && searchTerm.length > 0) {
+      if(m.title.toLowerCase().search(searchTerm) > -1) {
+        return true
+      }
+
+      return false
+    }
+
+    return true
+
+  }
+
   renderSelectMovie = movies => {
     const { meta } = this.props;
+
 
     return (
       <div>
         <Header>Skapa besök</Header>
+        <Field text="Namn:">
+          <Input
+            type="text"
+            onChange={v => this.setSearchTerm(v)}
+          />
+        </Field>
         Senaste uppdatering från SF:{" "}
         {(!meta.timestamp && "aldrig") ||
           moment(meta.timestamp).format("YYYY-MM-DD HH:mm")}
         {this.renderRequestButton()}
-        {sortBy(movies, "releaseDate").map(m =>
+        {sortBy(movies, "releaseDate").filter((m) => this.searchFilter(m)).map(m =>
           <Movie
             key={m.id}
             movie={m}
