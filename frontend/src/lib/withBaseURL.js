ae168b01a1a25af8460dcbe24176a69ba68eab01
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-07-03 22:30:21 +0200

    Change name of env variable

--- frontend/src/lib/withBaseURL.js
+++ frontend/src/lib/withBaseURL.js
@@ -1,4 +1,4 @@
-// Import BASE_URL from .env
-export const BASE_URL = process.env.BASE_URL;
+// Import REACT_APP_BASE_URL from .env
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const BASE_API_URL = `${BASE_URL}/api`;
 export const withBaseURL = path => BASE_API_URL + path;
