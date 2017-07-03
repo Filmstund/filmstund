c2e37538c1aa18ec74a587e7f02cd61c3b335742
Josefina Andreasson <josefina.andreasson@gmail.com>
2017-07-03 21:41:44 +0200

    Fancier login screen (#88)

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import styled from "styled-components";
 import { BASE_URL } from "../lib/withBaseURL";
+import background from "../assets/body_background.jpg";
+
+const LoginContainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${background});
+  background-size: cover;
+`;
+
+const LoginDialog = styled.div`
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.25);
+  padding: 2em;
+  background-color: #fff;
+  border: solid 1px #9e9e9e;
+  border-radius: 3px;
+  text-align: center;
+`;
 
 const GoogleButton = styled.button`
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .25);
-  font-family: Roboto;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.25);
+  padding: 0.5em 1em;
+  font-family: Roboto, sans-serif;
+  font-size: 12pt;
   background-color: #fff;
-  color: #757575;
+  color: #212121;
+  cursor: pointer;
+  border: solid 1px #9e9e9e;
+  border-radius: 3px;
+  
+  &:hover {
+    background-color: #fafafa;
+  }
+`;
+
+const GoogleLogo = styled.img`
+  display: inline;
+  height: 2em;
+  vertical-align: middle;
+  margin-right: 0.7em;
 `;
 
 class Login extends Component {
@@ -21,16 +55,18 @@ class Login extends Component {
   render() {
     const { className } = this.props;
     return (
-      <div className={className}>
-        <div>Logga in för att boka biobesök!</div>
-        <GoogleButton onClick={this.handleGoogleRedirect}>
-          Logga in via Google
-        </GoogleButton>
-      </div>
+      <LoginContainer className={className}>
+        <LoginDialog className={className}>
+          <img src={require('../assets/logo.png')} />
+          <h3>Logga in för att boka biobesök!</h3>
+          <GoogleButton onClick={this.handleGoogleRedirect}>
+            <GoogleLogo src={require('../assets/google-logo.svg')} />
+            Logga in via Google
+          </GoogleButton>
+        </LoginDialog>
+      </LoginContainer>
     );
   }
 }
 
-export default connect(state => ({
-  me: state.me
-}))(Login);
+export default Login;
