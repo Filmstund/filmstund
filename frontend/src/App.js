ec8acf76634e6c790343ba84899b366e2f99b719
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-07-09 16:26:03 +0200

    Make Login wrapping component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 import { Provider, connect } from "react-redux";
 import { me, meta } from "./store/reducers";
@@ -25,13 +25,6 @@ const ScrollContainer = styled.div`
   flex-direction: column;
 `;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: white;
-  height: 100vh;
-`;
-
 const AsyncHome = asyncComponent(() => import("./routes/Home"));
 const AsyncUser = asyncComponent(() => import("./routes/User"));
 const AsyncShowings = asyncComponent(() => import("./routes/Showings"));
@@ -49,26 +42,14 @@ class App extends Component {
     const { status } = this.props;
     const signedIn = status.data.id !== undefined;
 
-    if (!signedIn) {
-      return (
-        <Container>
-          <Login />
-        </Container>
-      );
-    }
-
     return (
       <ConnectedRouter history={history}>
-        <Container>
+        <Login signedIn={signedIn}>
           <TopBar />
           <ScrollContainer>
             <PaddingContainer>
               <Switch>
                 <Route exact path="/" component={AsyncHome} />
-                <Route
-                  path="/login"
-                  render={() => (signedIn ? <Redirect to="/" /> : <Login />)}
-                />
                 <Route path="/user" component={AsyncUser} />
                 <Route exact path="/showings" component={AsyncShowings} />
                 <Route
@@ -83,7 +64,7 @@ class App extends Component {
             </PaddingContainer>
             <Footer />
           </ScrollContainer>
-        </Container>
+        </Login>
       </ConnectedRouter>
     );
   }
--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { withRouter } from "react-router-dom";
 import { BASE_URL } from "../lib/withBaseURL";
+import qs from "qs";
 import background from "../assets/body_background.jpg";
 
 const LoginContainer = styled.div`
@@ -12,8 +14,15 @@ const LoginContainer = styled.div`
   background-size: cover;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: white;
+  height: 100vh;
+`;
+
 const LoginDialog = styled.div`
-  box-shadow: 0 2px 4px 0 rgba(0,0,0,.25);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .25);
   padding: 2em;
   background-color: #fff;
   border: solid 1px #9e9e9e;
@@ -22,7 +31,7 @@ const LoginDialog = styled.div`
 `;
 
 const GoogleButton = styled.button`
-  box-shadow: 0 2px 4px 0 rgba(0,0,0,.25);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .25);
   padding: 0.5em 1em;
   font-family: Roboto, sans-serif;
   font-size: 12pt;
@@ -31,7 +40,7 @@ const GoogleButton = styled.button`
   cursor: pointer;
   border: solid 1px #9e9e9e;
   border-radius: 3px;
-  
+
   &:hover {
     background-color: #fafafa;
   }
@@ -44,29 +53,47 @@ const GoogleLogo = styled.img`
   margin-right: 0.7em;
 `;
 
+const getRedirectUrl = searchString => {
+  if (searchString && searchString.length > 0) {
+    return qs.parse(searchString.substr(1)).return_to;
+  }
+};
+
 class Login extends Component {
   handleGoogleRedirect = () => {
+    const redirectParam = getRedirectUrl(this.props.location.search);
+
     window.location =
       BASE_URL +
-      "/login/google?redirect=" +
-      encodeURIComponent(window.location.pathname);
+      `/login/google?redirect=${encodeURIComponent(redirectParam || "/")}`;
   };
 
   render() {
-    const { className } = this.props;
+    const { children, className, signedIn } = this.props;
+
+    if (signedIn) {
+      return (
+        <Container>
+          {children}
+        </Container>
+      );
+    }
+
     return (
-      <LoginContainer className={className}>
-        <LoginDialog className={className}>
-          <img src={require('../assets/logo.png')} alt="IT-bio logga" />
-          <h3>Logga in för att boka biobesök!</h3>
-          <GoogleButton onClick={this.handleGoogleRedirect}>
-            <GoogleLogo src={require('../assets/google-logo.svg')} />
-            Logga in via Google
-          </GoogleButton>
-        </LoginDialog>
-      </LoginContainer>
+      <Container>
+        <LoginContainer className={className}>
+          <LoginDialog className={className}>
+            <img src={require("../assets/logo.png")} alt="IT-bio logga" />
+            <h3>Logga in för att boka biobesök!</h3>
+            <GoogleButton onClick={this.handleGoogleRedirect}>
+              <GoogleLogo src={require("../assets/google-logo.svg")} />
+              Logga in via Google
+            </GoogleButton>
+          </LoginDialog>
+        </LoginContainer>
+      </Container>
     );
   }
 }
 
-export default Login;
+export default withRouter(Login);
