2fa5122784bc382d8afdd1bf7c60e45a7a80abb2
Johan Horvius <horv@chalmers.it>
2017-07-10 17:39:51 +0200

    Some tweaking

--- frontend/src/ProjectorLoader.js
+++ frontend/src/ProjectorLoader.js
@@ -3,26 +3,22 @@ import React from "react"
 import styled from "styled-components";
 import loading from "./assets/loading.gif";
 
-
-const ratioHeight = 96/150;
 const ratioWidth = 150/96;
 
 const LoadingImage = styled.div`
     background-image: url(${props => props.src});
     background-size: cover;
-    height: ${props => props.height};
-    width: ${props => props.width};    
+    height: ${props => props.height}px;
+    width: ${props => props.width}px;    
     margin: 0 auto;
 `;
 
 
-const ProjectorLoader = ({height = '96px', width = '150px'}) => {
-
-  if(height !== '96px') {
-    width = (parseInt(height.replace('px', '')) * ratioWidth) + 'px';
-  } else if (width !== '96px') {
-    height = (parseInt(width.replace('px', '')) * ratioHeight) + 'px';
-  } 
+const ProjectorLoader = ({height = 96}) => {
+  let width = 150;
+  if(height !== 96) {
+    width = height * ratioWidth
+  }
   
   return (<span >
       <LoadingImage src={loading} height={height} width={width} />
