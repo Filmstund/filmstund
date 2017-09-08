345ec7fd653a4c3b38fbb4479735a791ed687d58
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-09-08 16:06:22 +0200

    Use renderComponent for Loader

--- frontend/src/loaders/UserLoader.js
+++ frontend/src/loaders/UserLoader.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { compose, lifecycle, branch } from "recompose";
+import { compose, lifecycle, branch, renderComponent } from "recompose";
 import * as fetchers from "../lib/fetchers";
 import Loader from "../ProjectorLoader";
 
@@ -19,7 +19,10 @@ const withUserData = lifecycle({
 const userIsUndefined = ({ user }) => user === undefined;
 
 // Will show Loader if user is undefined
-const showLoaderWhileFetching = branch(userIsUndefined, Loader);
+const showLoaderWhileFetching = branch(
+  userIsUndefined,
+  renderComponent(Loader)
+);
 
 // Combine all of the HOCs into a single enhancer
 export default compose(selectUser, withUserData, showLoaderWhileFetching);
