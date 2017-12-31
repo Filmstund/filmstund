b724b3865221c90a19aa1814c5a8fc937d9043d9
Emil Edholm <bobby@verypublic.org>
2017-12-31 12:05:36 +0100

    Cleanup TicketController
    Move all logic to the service

--- backend/src/main/kotlin/rocks/didit/sefilm/web/controllers/TicketController.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/web/controllers/TicketController.kt
@@ -1,90 +1,21 @@
 package rocks.didit.sefilm.web.controllers
 
 import org.springframework.http.MediaType
-import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.*
 import rocks.didit.sefilm.Application
-import rocks.didit.sefilm.NotFoundException
-import rocks.didit.sefilm.UserHasNotPaidException
-import rocks.didit.sefilm.currentLoggedInUser
-import rocks.didit.sefilm.database.entities.Ticket
-import rocks.didit.sefilm.database.repositories.ParticipantPaymentInfoRepository
-import rocks.didit.sefilm.database.repositories.ShowingRepository
-import rocks.didit.sefilm.database.repositories.TicketRepository
-import rocks.didit.sefilm.domain.UserID
-import rocks.didit.sefilm.domain.dto.TicketRange
 import rocks.didit.sefilm.web.services.TicketService
-import java.time.LocalDate
 import java.util.*
 
 @RestController
 @RequestMapping(Application.API_BASE_PATH + "/tickets")
-class TicketController(private val ticketRepository: TicketRepository,
-                       private val showingRepository: ShowingRepository,
-                       private val ticketService: TicketService,
-                       private val paymentInfoRepository: ParticipantPaymentInfoRepository) {
-
-  @GetMapping("/", produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)])
-  fun allMyTickets(): List<Ticket> {
-    val currentLoggedInUser = currentLoggedInUser()
-    val now = LocalDate.now()
-    return ticketRepository.findByAssignedToUser(currentLoggedInUser)
-      .filter {
-        it.date.isEqual(now) || it.date.isAfter(now)
-      }
-  }
+class TicketController(private val ticketService: TicketService) {
 
   @GetMapping("/{showingId}", produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)])
-  fun myTickets(@PathVariable showingId: UUID): List<Ticket> {
-    val currentLoggedInUser = currentLoggedInUser()
-    assertPaidAndEnrolled(showingId, currentLoggedInUser)
-
-    return ticketRepository.findByShowingIdAndAssignedToUser(showingId, currentLoggedInUser)
-  }
+  fun myTickets(@PathVariable showingId: UUID) = ticketService.getTicketsForCurrentUserAndShowing(showingId)
 
   @GetMapping("/{showingId}/seating-range", produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)])
-  fun seatingRange(@PathVariable showingId: UUID): TicketRange {
-    val currentLoggedInUser = currentLoggedInUser()
-    assertPaidAndEnrolled(showingId, currentLoggedInUser)
+  fun seatingRange(@PathVariable showingId: UUID) = ticketService.getTicketRange(showingId)
 
-    val allSeatsForShowing = ticketRepository.findByShowingId(showingId)
-      .map { it.seat }
-      .sortedBy { it.number }
-
-    val rows = allSeatsForShowing
-      .map { it.row }
-      .distinct()
-      .sorted()
-
-    val groupedSeats = allSeatsForShowing.groupBy({ it.row }, { it.number })
-    return TicketRange(rows, groupedSeats, allSeatsForShowing.size)
-  }
-
-  @PostMapping("/{showingId}",
-    consumes = [(MediaType.APPLICATION_JSON_UTF8_VALUE)],
-    produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)])
-  fun processTickets(@PathVariable showingId: UUID, @RequestBody urls: List<String>): List<Ticket> {
-    val currentLoggedInUser = currentLoggedInUser()
-    val showing = showingRepository.findById(showingId)
-      .orElseThrow { NotFoundException("showing with id $showingId") }
-
-    if (showing.admin != currentLoggedInUser) {
-      throw AccessDeniedException("Only the showing admin is allowed to do that")
-    }
-
-    ticketService.processTickets(urls, showing)
-    return myTickets(showingId)
-  }
-
-  private fun assertPaidAndEnrolled(showingId: UUID, currentLoggedInUser: UserID) {
-    val participant = paymentInfoRepository
-      .findByShowingIdAndUserId(showingId, currentLoggedInUser)
-      .orElseThrow {
-        throw NotFoundException("user. Not enrolled on this showing")
-      }
-
-    if (!participant.hasPaid) {
-      throw UserHasNotPaidException("User has not paid for this showing")
-    }
-  }
+  @PostMapping("/{showingId}", consumes = [(MediaType.APPLICATION_JSON_UTF8_VALUE)], produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)])
+  fun processTickets(@PathVariable showingId: UUID, @RequestBody urls: List<String>) = ticketService.processTickets(urls, showingId)
 }
