70ede3542ba2df1b0f39c638fc12ffc1074897bd
Emil Edholm <bobby@verypublic.org>
2018-01-04 01:31:21 +0100

    Ability to fetch SF-cities

---
+++ backend/src/main/kotlin/rocks/didit/sefilm/domain/dto/SfCityDTO.kt
@@ -0,0 +1,3 @@
+package rocks.didit.sefilm.domain.dto
+
+data class SfCityAliasDTO(val name: String, val alias: String)
--- backend/src/main/kotlin/rocks/didit/sefilm/services/LocationService.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/services/LocationService.kt
@@ -1,12 +1,24 @@
 package rocks.didit.sefilm.services
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.stereotype.Component
 import rocks.didit.sefilm.database.entities.Location
 import rocks.didit.sefilm.database.repositories.LocationRepository
+import rocks.didit.sefilm.domain.dto.SfCityAliasDTO
 import java.util.*
 
 @Component
 class LocationService(private val locationRepo: LocationRepository) {
   fun allLocations() = locationRepo.findAll().toList()
   fun getLocation(id: String): Optional<Location> = locationRepo.findById(id)
+
+  fun sfCities(): List<SfCityAliasDTO> {
+    val objectMapper: ObjectMapper = Jackson2ObjectMapperBuilder.json().build()
+
+    val cityResource = ClassPathResource("seeds/sf-cities.json")
+    return objectMapper.readValue(cityResource.inputStream)
+  }
 }
