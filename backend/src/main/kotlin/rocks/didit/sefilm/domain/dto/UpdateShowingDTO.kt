af91396d64e35d18ebff054cbcd78ec646a331f4
Emil Edholm <bobby@verypublic.org>
2018-01-05 00:28:23 +0100

    Update showing mutations

--- backend/src/main/kotlin/rocks/didit/sefilm/domain/dto/UpdateShowingDTO.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/domain/dto/UpdateShowingDTO.kt
@@ -5,10 +5,12 @@ import java.time.LocalTime
 
 data class UpdateShowingDTO(val price: Long = 0,
                             val private: Boolean = false,
+                            @Deprecated("Should be removed when we switch to graphql")
                             val ticketsBought: Boolean = false,
-                            val payToUser: String? = null,
+                            val payToUser: String = "N/A",
                             val expectedBuyDate: LocalDate? = null,
-                            val location: String? = null,
-                            val time: LocalTime? = null,
+                            val location: String = "Filmstaden Bergakungen",
+                            val time: LocalTime = LocalTime.NOON,
                             /** Links to SF tickets, i.e https://www.sf.se/bokning/mina-e-biljetter/Sys99-SE/AA-1033-201712132045/RE-XXXXXXXXXX */
+                            @Deprecated("Should be removed when we switch to graphql")
                             val cinemaTicketUrls: List<String> = listOf())
