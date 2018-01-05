cc1585e83285933b29535adecbeae21f49331b56
Emil Edholm <bobby@verypublic.org>
2018-01-05 12:24:53 +0100

    Use the Oauth2RestTemplate instead
    Also include source and description to the google calendar. Also
    correctly send email notifications

--- backend/src/main/kotlin/rocks/didit/sefilm/domain/dto/CalendarEventDTO.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/domain/dto/CalendarEventDTO.kt
@@ -8,17 +8,22 @@ data class CalendarEventDTO(val summary: String,
                             val location: String? = null,
                             val attendees: List<Map<String, String>>,
                             val start: Map<String, Serializable>,
-                            val end: Map<String, Serializable>) {
+                            val end: Map<String, Serializable>,
+                            val source: Map<String, String>,
+                            val description: String) {
 
   companion object Factory {
-    fun of(summary: String, location: Location?, emails: Collection<String>, start: ZonedDateTime, end: ZonedDateTime): CalendarEventDTO {
+    fun of(summary: String, location: Location?, emails: Collection<String>, start: ZonedDateTime, end: ZonedDateTime,
+           sfBuyLink: String = "https://www.sf.se", description: String = "$summary @ ${location?.name}\n$sfBuyLink"): CalendarEventDTO {
 
       return CalendarEventDTO(
         summary = summary,
         location = location?.name ?: "N/A",
         attendees = emails.map { mapOf("email" to it) },
         start = mapOf("dateTime" to start.toOffsetDateTime().toString()),
-        end = mapOf("dateTime" to end.toOffsetDateTime().toString())
+        end = mapOf("dateTime" to end.toOffsetDateTime().toString()),
+        source = mapOf("title" to "SF Bio", "url" to sfBuyLink),
+        description = description
       )
     }
   }
--- backend/src/main/kotlin/rocks/didit/sefilm/services/GoogleCalenderService.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/services/GoogleCalenderService.kt
@@ -2,65 +2,48 @@ package rocks.didit.sefilm.services
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
-import org.springframework.security.oauth2.common.OAuth2AccessToken
+import org.springframework.security.oauth2.client.OAuth2RestTemplate
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
 import rocks.didit.sefilm.domain.dto.CalendarEventDTO
-import rocks.didit.sefilm.oauthAccessToken
 
 @Component
 class GoogleCalenderService(
-  private val restTemplate: RestTemplate) {
+  private val restTemplate: OAuth2RestTemplate) {
 
   companion object {
     private const val API_URL = "https://www.googleapis.com/calendar/v3/calendars/primary/events"
     private val log: Logger = LoggerFactory.getLogger(GoogleCalenderService::class.java)
   }
 
-  @Value("\${google.clientSecret}")
-  private val clientSecret: String? = null
-
   /** @return the event id of the newly created event */
   fun createEvent(event: CalendarEventDTO): String {
-    val accessToken = oauthAccessToken()
+    val entity = HttpEntity(event)
 
-    val entity = HttpEntity(event, headersWithToken(accessToken))
+    val uri = UriComponentsBuilder.fromUriString(API_URL)
+      .queryParam("sendNotifications", "true")
+      .build().toUri()
 
     val resultBody: Map<String, Any?>? = restTemplate
-      .exchange(API_URL,
+      .exchange(uri,
         HttpMethod.POST,
         entity,
-        object : ParameterizedTypeReference<Map<String, Any?>>() {},
-        mapOf("sendNotifications" to "true", "key" to clientSecret))
+        object : ParameterizedTypeReference<Map<String, Any?>>() {})
       .body
 
     return resultBody?.get("id")?.toString() ?: ""
   }
 
   fun deleteEvent(eventId: String) {
-    val accessToken = oauthAccessToken()
-    val headers = headersWithToken(accessToken)
-    val entity = HttpEntity<Void>(headers)
-
     try {
       log.info("Removing calendar event with id: $eventId")
-      restTemplate.exchange("https://www.googleapis.com/calendar/v3/calendars/primary/events/{eventId}", HttpMethod.DELETE, entity, Void::class.java, eventId)
+      restTemplate.delete("$API_URL/{eventId}", eventId)
     } catch (e: Exception) {
       log.warn("Unable to delete calendar event with ID: $eventId", e)
     }
-  }
-
-  private fun headersWithToken(accessToken: OAuth2AccessToken): HttpHeaders {
-    val headers = HttpHeaders()
-    headers.contentType = APPLICATION_JSON_UTF8
-    headers.set("authorization", accessToken.tokenType + " " + accessToken.value)
-    return headers
   }
 }
 
