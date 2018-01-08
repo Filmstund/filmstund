aa07b04f12d7069a378bfcc229141d645d220493
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-01-08 21:27:44 +0100

    Fix user profile

--- frontend/src/routes/SingleShowing/AdminAction.js
+++ frontend/src/routes/SingleShowing/AdminAction.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
 
-import { jsonRequest } from "../../lib/fetch";
-import { withBaseURL } from "../../lib/withBaseURL";
-
 import MainButton, { GrayButton, RedButton } from "../../MainButton";
 import BuyModal from "./BuyModal";
 import gql from "graphql-tag";
