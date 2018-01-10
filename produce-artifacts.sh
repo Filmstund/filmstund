7617128ce593a90ec97384df286fdbdbd042d2b8
Emil Edholm <bobby@verypublic.org>
2018-01-10 21:29:59 +0100

    Include version in frontend build

--- produce-artifacts.sh
+++ produce-artifacts.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
+latestVersion=`git describe`
 mkdir -p artifacts
 cd frontend
 yarn
 yarn build
-apack ../artifacts/frontend.tar.gz build/
-rm -r build/
+mv build/ sefilm/
+apack ../artifacts/frontend-$latestVersion.tar.gz sefilm/
+rm -r sefilm/
 
 cd ../backend
 ./gradlew clean build
