0204f4dad8d3e4993d326722f4c98c016e41c775
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-02-03 00:03:59 +0100

    minmax instead of mediaquery

--- frontend/src/ShowingsGrid.js
+++ frontend/src/ShowingsGrid.js
@@ -2,13 +2,6 @@ import styled from "styled-components";
 
 export const ShowingsGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 10px;
-
-  @media (min-width: 40rem) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (min-width: 60rem) {
-    grid-template-columns: repeat(3, 1fr);
-  }
 `;
