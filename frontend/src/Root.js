288f1aa8134202b4162b671e25cb76faff18fa29
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-08-02 15:52:37 +0200

    Import App with asyncComponent

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
 import client from "./store/apollo";
 import Login from "./use-cases/login/containers/Login";
-import App from "./App";
+import asyncComponent from "./use-cases/common/utils/AsyncComponent";
+import { rollbar } from "./lib/error-reporting";
 
-const Root = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <Login>{props => <App {...props} />}</Login>
-    </Router>
-  </ApolloProvider>
-);
+const AsyncApp = asyncComponent(() => import("./App"));
+
+class Root extends Component {
+    state = {
+        hasError: false
+    };
+
+    componentDidCatch(error, info) {
+        rollbar.error("Root didCatch", error, info);
+        this.setState({
+            hasError: true
+        })
+    }
+
+    render() {
+        const { hasError } = this.state;
+
+        if (hasError) {
+            return "Something went wrong!";
+        }
+
+        return (
+            <ApolloProvider client={client}>
+                <Router>
+                    <Login>{props => <AsyncApp {...props} />}</Login>
+                </Router>
+            </ApolloProvider>
+        );
+    }
+}
 
 export default Root;
