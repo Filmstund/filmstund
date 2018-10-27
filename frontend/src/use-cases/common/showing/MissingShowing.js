57e4acff5ea8538c68036daf18d1257b78c6cf8a
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-10-27 23:28:40 +0200

    Add MissingShowing view

---
+++ frontend/src/use-cases/common/showing/MissingShowing.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { PageWidthWrapper } from "../ui/PageWidthWrapper";
+
+import alfons from "../../../assets/alfons.jpg";
+import Header, { SmallHeader } from "../ui/Header";
+
+export const MissingShowing = () => (
+  <PageWidthWrapper>
+    <div style={{ backgroundColor: "#fff" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Header>Det finns ingen sådan visning</Header>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <SmallHeader>Kanske någon har raderat den...?</SmallHeader>
+        <div
+          style={{
+            alignSelf: "flex-end",
+            backgroundImage: `url(${alfons})`,
+            height: 50,
+            width: 200
+          }}
+        />
+      </div>
+    </div>
+  </PageWidthWrapper>
+);
