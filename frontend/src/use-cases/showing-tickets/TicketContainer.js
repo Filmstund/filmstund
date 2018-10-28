2af241935e97e720978e2ab93adefb0797f40096
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-10-28 13:09:17 +0100

    Refactor TicketContainer using hooks

--- frontend/src/use-cases/showing-tickets/TicketContainer.js
+++ frontend/src/use-cases/showing-tickets/TicketContainer.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import Loader from "../../use-cases/common/utils/ProjectorLoader";
+import React, { useState, useCallback } from "react";
 import TicketURLInput from "../../use-cases/common/ui/TicketURLInput";
 import MainButton from "../../use-cases/common/ui/MainButton";
 import Ticket from "./Ticket";
@@ -11,92 +10,59 @@ import { SmallHeader } from "../../use-cases/common/ui/Header";
 import { navigateToShowing } from "../common/navigators/index";
 
 import { FieldWithoutMaxWidth } from "../common/ui/Field";
+import { useApolloMutationResult } from "../common/utils/useApolloMutationResult";
 
-export default class TicketContainer extends Component {
-  state = {
-    errors: null,
-    success: false,
-    cinemaTicketUrls: []
-  };
+const TicketContainer = props => {
+  const {
+    data: { me, showing },
+    history
+  } = props;
 
-  handleSubmitCinemaTicketUrls = () => {
-    const { cinemaTicketUrls } = this.state;
-    const {
-      data: { showing }
-    } = this.props;
+  const [cinemaTicketUrls, setCinemaTicketUrls] = useState([]);
+  const { errors, success, mutate: addTickets } = useApolloMutationResult(
+    props.addTickets
+  );
 
+  const handleGoBackToShowing = useCallback(() => {
+    navigateToShowing(history, showing);
+  });
+
+  const handleSubmitCinemaTicketUrls = useCallback(() => {
     const nonEmptyUrls = cinemaTicketUrls.filter(
       line => line.trim().length !== 0
     );
 
-    this.props
-      .addTickets(showing.id, nonEmptyUrls)
-      .then(() => {
-        this.setState({ success: true, errors: null, cinemaTicketUrls: [] });
-      })
-      .catch(errors => {
-        this.setState({ success: false, errors });
-      });
-  };
+    addTickets(showing.id, nonEmptyUrls).then(() => setCinemaTicketUrls([]));
+  });
 
-  setCinemaTicketUrls = cinemaTicketUrls => {
-    this.setState({
-      cinemaTicketUrls
-    });
-  };
-
-  renderAdminFields = () => {
-    const { cinemaTicketUrls } = this.state;
-
-    return (
-      <FieldWithoutMaxWidth text="Lägg till SF-bokningslänkar">
-        <TicketURLInput
-          cinemaTicketUrls={cinemaTicketUrls}
-          onChange={this.setCinemaTicketUrls}
-        />
-        <MainButton onClick={this.handleSubmitCinemaTicketUrls}>
-          Skicka
-        </MainButton>
-      </FieldWithoutMaxWidth>
-    );
-  };
-
-  handleGoBackToShowing = () => {
-    const {
-      history,
-      data: { showing }
-    } = this.props;
-    navigateToShowing(history, showing);
-  };
-
-  render() {
-    const { errors, success } = this.state;
-    const {
-      data: { me, showing, loading }
-    } = this.props;
-
-    const { myTickets, ticketRange, sfSeatMap } = showing;
-
-    if (loading) {
-      return <Loader />;
-    } else {
-      return (
-        <PageWidthWrapper>
-          <MainButton onClick={this.handleGoBackToShowing}>
-            Tillbaka till visning
-          </MainButton>
-          <SmallHeader>Våra platser:</SmallHeader>
-          <SeatRange ticketRange={ticketRange} />
-          <ScreenSeats ticketRange={ticketRange} seatMap={sfSeatMap} />
-          {myTickets.map(ticket => <Ticket key={ticket.id} {...ticket} />)}
-          <StatusMessageBox
-            success={success}
-            errors={errors}
-            successMessage="Uppdaterades!"
+  const { myTickets, ticketRange, sfSeatMap } = showing;
+  return (
+    <PageWidthWrapper>
+      <MainButton onClick={handleGoBackToShowing}>
+        Tillbaka till visning
+      </MainButton>
+      <SmallHeader>Våra platser:</SmallHeader>
+      <SeatRange ticketRange={ticketRange} />
+      <ScreenSeats ticketRange={ticketRange} seatMap={sfSeatMap} />
+      {myTickets.map(ticket => (
+        <Ticket key={ticket.id} {...ticket} />
+      ))}
+      <StatusMessageBox
+        success={success}
+        errors={errors}
+        successMessage="Uppdaterades!"
+      />
+      {showing.admin.id === me.id && (
+        <FieldWithoutMaxWidth text="Lägg till SF-bokningslänkar">
+          <TicketURLInput
+            cinemaTicketUrls={cinemaTicketUrls}
+            onChange={setCinemaTicketUrls}
           />
-          {showing.admin.id === me.id && this.renderAdminFields()}
-        </PageWidthWrapper>
-      );
-    }
-  }
-}
+          <MainButton onClick={handleSubmitCinemaTicketUrls}>Skicka</MainButton>
+        </FieldWithoutMaxWidth>
+      )}
+    </PageWidthWrapper>
+  );
+};
+
+export default TicketContainer;
