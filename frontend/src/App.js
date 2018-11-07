af35c848e89b55830f03b33acfcae24fc03903c2
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-11-07 21:43:05 +0100

    Scroll in body

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,6 @@ import Loader from "./use-cases/common/utils/ProjectorLoader";
 import { MissingShowing } from "./use-cases/common/showing/MissingShowing";
 import { PageTitleTemplate } from "./use-cases/common/utils/PageTitle";
 
-const ScrollContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  overflow-y: scroll;
-  -webkit-overflow-scrolling: touch;
-`;
-
 const MainGridContainer = styled.div`
   flex: 1;
   grid-area: content;
@@ -57,36 +49,34 @@ const App = ({ data: { me }, signout }) => (
   <>
     <PageTitleTemplate titleTemplate="%s | sefilm">
       <WelcomeModal me={me} />
-      <ScrollContainer>
-        <NavBar signout={signout} />
-        <MainGridContainer>
-          <Suspense fallback={<Loader />}>
-            <Switch>
-              <Route exact path="/" children={<AsyncHome />} />
-              <Route path="/user" children={<AsyncUser />} />
-              <Route exact path="/showings" children={<AsyncShowings />} />
-              <Route
-                path="/showings/new/:movieId?"
-                children={<AsyncNewShowing />}
-              />
-              <Route
-                path="/showings/:webId/:slug/tickets"
-                children={<AsyncShowingTickets />}
-              />
-              <Route
-                path="/showings/:webId/:slug/edit"
-                children={<AsyncEditShowing />}
-              />
-              <Route
-                path="/showings/:webId/:slug"
-                children={<AsyncSingleShowing />}
-              />
-              <Route children={<MissingShowing />} />
-            </Switch>
-          </Suspense>
-        </MainGridContainer>
-        <Footer />
-      </ScrollContainer>
+      <NavBar signout={signout} />
+      <MainGridContainer>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route exact path="/" children={<AsyncHome />} />
+            <Route path="/user" children={<AsyncUser />} />
+            <Route exact path="/showings" children={<AsyncShowings />} />
+            <Route
+              path="/showings/new/:movieId?"
+              children={<AsyncNewShowing />}
+            />
+            <Route
+              path="/showings/:webId/:slug/tickets"
+              children={<AsyncShowingTickets />}
+            />
+            <Route
+              path="/showings/:webId/:slug/edit"
+              children={<AsyncEditShowing />}
+            />
+            <Route
+              path="/showings/:webId/:slug"
+              children={<AsyncSingleShowing />}
+            />
+            <Route children={<MissingShowing />} />
+          </Switch>
+        </Suspense>
+      </MainGridContainer>
+      <Footer />
     </PageTitleTemplate>
   </>
 );
