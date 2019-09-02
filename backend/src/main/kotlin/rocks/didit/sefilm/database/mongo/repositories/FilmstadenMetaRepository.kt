283163d132b8af3dfad8fd86630a5e64b87ef355
Emil Edholm <bobby@verypublic.org>
2019-09-02 19:46:43 +0200

    Remove unsued entity, repo (FilmstadenMeta)

--- backend/src/main/kotlin/rocks/didit/sefilm/database/mongo/repositories/FilmstadenMetaRepository.kt
+++
@@ -1,8 +0,0 @@
-package rocks.didit.sefilm.database.mongo.repositories
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import rocks.didit.sefilm.database.mongo.entities.FilmstadenMeta
-
-@Repository
-interface FilmstadenMetaRepository : CrudRepository<FilmstadenMeta, String>
