285c0295a23a2069065051eaf9837b3f980171ef
Emil Edholm <bobby@verypublic.org>
2019-09-08 16:05:19 +0200

    Fix Location property expression
    Now the repository actually works

--- backend/src/main/kotlin/rocks/didit/sefilm/database/repositories/LocationRepository.kt
+++ backend/src/main/kotlin/rocks/didit/sefilm/database/repositories/LocationRepository.kt
@@ -3,8 +3,10 @@ package rocks.didit.sefilm.database.repositories
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import rocks.didit.sefilm.database.entities.Location
+import rocks.didit.sefilm.database.entities.LocationAlias
+import java.util.*
 
 @Repository
 interface LocationRepository : JpaRepository<Location, String> {
-  //fun findByNameIgnoreCaseOrAliasIgnoreCase(id: String, alias: String): Optional<Location>
+  fun findByNameIgnoreCaseOrAliasAliasIgnoreCase(name: String, alias_alias: LocationAlias): Optional<Location>
 }
