e4aadafb830057d14fed721fedd0bec897db6413
Johan Lindskogen <johan.lindskogen@gmail.com>
2019-09-11 22:37:42 +0200

    Fix gesture-handler crash

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
+import "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
-import { useScreens } from 'react-native-screens';
+import { useScreens } from "react-native-screens";
+
 useScreens();
 
 AppRegistry.registerComponent(appName, () => App);
