4acc3412d5d18c6f105f1e16d1b6176492511cde
Emil Edholm <bobby@verypublic.org>
2019-10-27 18:44:19 +0100

    Fix deserialization issue on TicketNumber

--- backend/api/src/main/kotlin/se/filmstund/domain/id/TicketNumber.kt
+++ backend/api/src/main/kotlin/se/filmstund/domain/id/TicketNumber.kt
@@ -1,12 +1,20 @@
 package se.filmstund.domain.id
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
 
-data class TicketNumber(@JsonValue val number: String) {
+data class TicketNumber(val number: String) {
+  companion object {
+    @JvmStatic
+    @JsonCreator
+    fun from(str: String?) = str?.let { TicketNumber(it) }
+  }
+
   init {
     require(number.length == 11) { "Ticket number has wrong size. Expected 11, got ${number.length}" }
     requireNotNull(number.toLongOrNull()) { "'$number' is an invalid ticket number" }
   }
 
+  @JsonValue
   override fun toString() = number
 }
