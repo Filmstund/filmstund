2c313bb4576fe7a003849a0b715bd0f1a4e2165b
Emil Edholm <bobby@verypublic.org>
2021-09-19 14:16:31 +0200

    chore: add test for invalid db connections

---
+++ internal/database/connections_test.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"context"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestNew_invalid_connection(t *testing.T) {
+	t.Parallel()
+	_, err := New(context.TODO(), nil)
+	if err == nil {
+		t.Fatalf("expected err, got none")
+	}
+	assert.ErrorContains(t, err, "failed to initiate database connection pool: failed to connect to")
+}
