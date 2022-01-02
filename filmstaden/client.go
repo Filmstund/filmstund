b004ac1b05ad275ce4d0047ec61a31a9ef690b0f
Emil Edholm <bobby@verypublic.org>
2022-01-02 16:03:58 +0100

    feat(filmstaden): add default headers to requests
    User-Agent, Accept, and X-Requested-With headers are now automatically
    added when using the http client.

--- filmstaden/client.go
+++ filmstaden/client.go
@@ -6,8 +6,9 @@ import (
 )
 
 const (
-	APIURL         = "https://www.filmstaden.se/api/v2"
-	requestTimeout = 3 * time.Second
+	APIURL           = "https://www.filmstaden.se/api/v2"
+	requestTimeout   = 3 * time.Second
+	defaultUserAgent = "Mozilla/5.0 (Linux; Android 11; Pixel 3 XL Build/RQ1A.210205.004; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36"
 )
 
 type Client struct {
@@ -16,10 +17,25 @@ type Client struct {
 }
 
 func NewClient(baseURL string) *Client {
+	transport := defaultHeaderTransport{
+		roundTripper: http.DefaultTransport,
+	}
 	return &Client{
 		baseURL: baseURL,
 		http: &http.Client{
-			Timeout: requestTimeout,
+			Transport: &transport,
+			Timeout:   requestTimeout,
 		},
 	}
+}
+
+type defaultHeaderTransport struct {
+	roundTripper http.RoundTripper
+}
+
+func (transport *defaultHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("User-Agent", defaultUserAgent)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("X-Requested-With", "se.sfbio.mobile.android")
+	return transport.roundTripper.RoundTrip(req)
 }
