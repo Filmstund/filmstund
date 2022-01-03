534ad283fd7aca1003c3d2ec6fd81b93c6d6280d
Emil Edholm <bobby@verypublic.org>
2022-01-03 14:09:43 +0100

    feat(filmstaden): add Movies merge function
    this allows you to merge two movie structs into a single one that only
    contains all unique movies.

--- filmstaden/get_movies.go
+++ filmstaden/get_movies.go
@@ -6,148 +6,173 @@ import (
 	"strings"
 )
 
-type Movies struct {
-	TotalNbrOfItems int `json:"totalNbrOfItems"`
-	Items           []struct {
-		Versions []struct {
-			Title             string  `json:"title"`
-			Slug              string  `json:"slug"`
-			LanguageID        string  `json:"languageId"`
-			ReleaseDate       string  `json:"releaseDate"`
-			AudioLanguage     *string `json:"audioLanguage"`
-			AudioLanguageInfo *struct {
-				DisplayName                string `json:"displayName"`
-				Alias                      string `json:"alias"`
-				Name                       string `json:"name"`
-				ThreeLetterISOLanguageName string `json:"threeLetterISOLanguageName"`
-				Description                string `json:"description"`
-			} `json:"audioLanguageInfo"`
-			SubtitlesLanguage     *string `json:"subtitlesLanguage"`
-			SubtitlesLanguageInfo *struct {
-				DisplayName                string `json:"displayName"`
-				Alias                      string `json:"alias"`
-				Name                       string `json:"name"`
-				ThreeLetterISOLanguageName string `json:"threeLetterISOLanguageName"`
-				Description                string `json:"description"`
-			} `json:"subtitlesLanguageInfo"`
-			Attributes []struct {
-				Alias       string  `json:"alias"`
-				DisplayName string  `json:"displayName"`
-				Description *string `json:"description"`
-			} `json:"attributes"`
-			NcgID             string `json:"ncgId"`
-			RemoteSystemAlias string `json:"remoteSystemAlias"`
-			RemoteEntityID    string `json:"remoteEntityId"`
-			Rating            struct {
-				DisplayName    string `json:"displayName"`
-				Alias          string `json:"alias"`
-				Age            int    `json:"age"`
-				AgeAccompanied int    `json:"ageAccompanied"`
-			} `json:"rating"`
-			AudioLanguages []struct {
-				DisplayName                string `json:"displayName"`
-				Alias                      string `json:"alias"`
-				Name                       string `json:"name"`
-				ThreeLetterISOLanguageName string `json:"threeLetterISOLanguageName"`
-				Description                string `json:"description"`
-			} `json:"audioLanguages"`
-			PartnerShow bool        `json:"partnerShow"`
-			PartnerCode interface{} `json:"partnerCode"`
-		} `json:"versions"`
-		NcgID             string `json:"ncgId"`
-		LanguageID        string `json:"languageId"`
-		Title             string `json:"title"`
-		OriginalTitle     string `json:"originalTitle"`
-		Slug              string `json:"slug"`
-		OriginalLanguage  string `json:"originalLanguage"`
-		OriginalLanguages []struct {
-			NativeName  string      `json:"nativeName"`
-			EnglishName string      `json:"englishName"`
-			DisplayName string      `json:"displayName"`
-			Alias       string      `json:"alias"`
-			Description interface{} `json:"description"`
-		} `json:"originalLanguages"`
-		ProductionYear         int    `json:"productionYear"`
-		ShortDescription       string `json:"shortDescription"`
-		LongDescription        string `json:"longDescription"`
-		LongDescriptionFormats []struct {
-			Type string `json:"type"`
-			Text string `json:"text"`
-		} `json:"longDescriptionFormats"`
-		ReleaseDate        string      `json:"releaseDate"`
-		ReleaseDateInfo    interface{} `json:"releaseDateInfo"`
-		PreReleaseDate     *string     `json:"preReleaseDate"`
-		PreReleaseDateInfo interface{} `json:"preReleaseDateInfo"`
-		ReReleaseDate      string      `json:"reReleaseDate"`
-		ReReleaseDateInfo  interface{} `json:"reReleaseDateInfo"`
-		Actors             []struct {
+type Movie struct {
+	Versions []struct {
+		Title             string  `json:"title"`
+		Slug              string  `json:"slug"`
+		LanguageID        string  `json:"languageId"`
+		ReleaseDate       string  `json:"releaseDate"`
+		AudioLanguage     *string `json:"audioLanguage"`
+		AudioLanguageInfo *struct {
+			DisplayName                string `json:"displayName"`
+			Alias                      string `json:"alias"`
+			Name                       string `json:"name"`
+			ThreeLetterISOLanguageName string `json:"threeLetterISOLanguageName"`
+			Description                string `json:"description"`
+		} `json:"audioLanguageInfo"`
+		SubtitlesLanguage     *string `json:"subtitlesLanguage"`
+		SubtitlesLanguageInfo *struct {
+			DisplayName                string `json:"displayName"`
+			Alias                      string `json:"alias"`
+			Name                       string `json:"name"`
+			ThreeLetterISOLanguageName string `json:"threeLetterISOLanguageName"`
+			Description                string `json:"description"`
+		} `json:"subtitlesLanguageInfo"`
+		Attributes []struct {
+			Alias       string  `json:"alias"`
 			DisplayName string  `json:"displayName"`
-			FirstName   string  `json:"firstName"`
-			LastName    *string `json:"lastName"`
-		} `json:"actors"`
-		Directors []struct {
-			DisplayName string `json:"displayName"`
-			FirstName   string `json:"firstName"`
-			LastName    string `json:"lastName"`
-		} `json:"directors"`
-		Roles []struct {
-			RoleName string `json:"roleName"`
-			Persons  []struct {
-				DisplayName string `json:"displayName"`
-				FirstName   string `json:"firstName"`
-				LastName    string `json:"lastName"`
-			} `json:"persons"`
-		} `json:"roles"`
-		Producers []interface{} `json:"producers"`
-		Genres    []struct {
-			Name string `json:"name"`
-		} `json:"genres"`
-		Categories []struct {
-			Alias       string `json:"alias"`
-			DisplayName string `json:"displayName"`
-			SortOrder   int    `json:"sortOrder"`
-		} `json:"categories"`
-		EntityReferences []struct {
-			EntityType string `json:"entityType"`
-			Direction  string `json:"direction"`
-			EntityID   string `json:"entityId"`
-		} `json:"entityReferences"`
-		Rating struct {
+			Description *string `json:"description"`
+		} `json:"attributes"`
+		NcgID             string `json:"ncgId"`
+		RemoteSystemAlias string `json:"remoteSystemAlias"`
+		RemoteEntityID    string `json:"remoteEntityId"`
+		Rating            struct {
 			DisplayName    string `json:"displayName"`
 			Alias          string `json:"alias"`
 			Age            int    `json:"age"`
 			AgeAccompanied int    `json:"ageAccompanied"`
 		} `json:"rating"`
-		Length       int `json:"length"`
-		VideoStreams []struct {
-			Category    string      `json:"category"`
-			URL         string      `json:"url"`
-			Description string      `json:"description"`
-			Image       interface{} `json:"image"`
-		} `json:"videoStreams"`
-		PosterURL string `json:"posterUrl"`
-		Images    []struct {
-			FileName     string      `json:"fileName"`
-			Size         int         `json:"size"`
-			Height       int         `json:"height"`
-			Width        int         `json:"width"`
-			Version      int         `json:"version"`
-			AltText      *string     `json:"altText"`
-			Caption      *string     `json:"caption"`
-			Copyright    interface{} `json:"copyright"`
-			ID           string      `json:"id"`
-			ImageType    string      `json:"imageType"`
-			ContentType  string      `json:"contentType"`
-			URL          string      `json:"url"`
-			Alternatives []struct {
-				ContentType string `json:"contentType"`
-				URL         string `json:"url"`
-			} `json:"alternatives"`
-		} `json:"images"`
-		SeoTitle       *string `json:"seoTitle"`
-		SeoDescription *string `json:"seoDescription"`
-		SpecialMovie   bool    `json:"specialMovie"`
-	} `json:"items"`
+		AudioLanguages []struct {
+			DisplayName                string `json:"displayName"`
+			Alias                      string `json:"alias"`
+			Name                       string `json:"name"`
+			ThreeLetterISOLanguageName string `json:"threeLetterISOLanguageName"`
+			Description                string `json:"description"`
+		} `json:"audioLanguages"`
+		PartnerShow bool        `json:"partnerShow"`
+		PartnerCode interface{} `json:"partnerCode"`
+	} `json:"versions"`
+	NcgID             string `json:"ncgId"`
+	LanguageID        string `json:"languageId"`
+	Title             string `json:"title"`
+	OriginalTitle     string `json:"originalTitle"`
+	Slug              string `json:"slug"`
+	OriginalLanguage  string `json:"originalLanguage"`
+	OriginalLanguages []struct {
+		NativeName  string      `json:"nativeName"`
+		EnglishName string      `json:"englishName"`
+		DisplayName string      `json:"displayName"`
+		Alias       string      `json:"alias"`
+		Description interface{} `json:"description"`
+	} `json:"originalLanguages"`
+	ProductionYear         int    `json:"productionYear"`
+	ShortDescription       string `json:"shortDescription"`
+	LongDescription        string `json:"longDescription"`
+	LongDescriptionFormats []struct {
+		Type string `json:"type"`
+		Text string `json:"text"`
+	} `json:"longDescriptionFormats"`
+	ReleaseDate        string      `json:"releaseDate"`
+	ReleaseDateInfo    interface{} `json:"releaseDateInfo"`
+	PreReleaseDate     *string     `json:"preReleaseDate"`
+	PreReleaseDateInfo interface{} `json:"preReleaseDateInfo"`
+	ReReleaseDate      string      `json:"reReleaseDate"`
+	ReReleaseDateInfo  interface{} `json:"reReleaseDateInfo"`
+	Actors             []struct {
+		DisplayName string  `json:"displayName"`
+		FirstName   string  `json:"firstName"`
+		LastName    *string `json:"lastName"`
+	} `json:"actors"`
+	Directors []struct {
+		DisplayName string `json:"displayName"`
+		FirstName   string `json:"firstName"`
+		LastName    string `json:"lastName"`
+	} `json:"directors"`
+	Roles []struct {
+		RoleName string `json:"roleName"`
+		Persons  []struct {
+			DisplayName string `json:"displayName"`
+			FirstName   string `json:"firstName"`
+			LastName    string `json:"lastName"`
+		} `json:"persons"`
+	} `json:"roles"`
+	Producers []interface{} `json:"producers"`
+	Genres    []struct {
+		Name string `json:"name"`
+	} `json:"genres"`
+	Categories []struct {
+		Alias       string `json:"alias"`
+		DisplayName string `json:"displayName"`
+		SortOrder   int    `json:"sortOrder"`
+	} `json:"categories"`
+	EntityReferences []struct {
+		EntityType string `json:"entityType"`
+		Direction  string `json:"direction"`
+		EntityID   string `json:"entityId"`
+	} `json:"entityReferences"`
+	Rating struct {
+		DisplayName    string `json:"displayName"`
+		Alias          string `json:"alias"`
+		Age            int    `json:"age"`
+		AgeAccompanied int    `json:"ageAccompanied"`
+	} `json:"rating"`
+	Length       int `json:"length"`
+	VideoStreams []struct {
+		Category    string      `json:"category"`
+		URL         string      `json:"url"`
+		Description string      `json:"description"`
+		Image       interface{} `json:"image"`
+	} `json:"videoStreams"`
+	PosterURL string `json:"posterUrl"`
+	Images    []struct {
+		FileName     string      `json:"fileName"`
+		Size         int         `json:"size"`
+		Height       int         `json:"height"`
+		Width        int         `json:"width"`
+		Version      int         `json:"version"`
+		AltText      *string     `json:"altText"`
+		Caption      *string     `json:"caption"`
+		Copyright    interface{} `json:"copyright"`
+		ID           string      `json:"id"`
+		ImageType    string      `json:"imageType"`
+		ContentType  string      `json:"contentType"`
+		URL          string      `json:"url"`
+		Alternatives []struct {
+			ContentType string `json:"contentType"`
+			URL         string `json:"url"`
+		} `json:"alternatives"`
+	} `json:"images"`
+	SeoTitle       *string `json:"seoTitle"`
+	SeoDescription *string `json:"seoDescription"`
+	SpecialMovie   bool    `json:"specialMovie"`
+}
+
+type Movies struct {
+	TotalCount int     `json:"totalNbrOfItems"`
+	Items      []Movie `json:"items"`
+}
+
+// Merge this Movies struct with another based on the movie ID (NCG ID).
+func (movies *Movies) Merge(other *Movies) *Movies {
+	if movies == nil || other == nil {
+		return movies
+	}
+	set := make(map[string]Movie, movies.TotalCount+other.TotalCount)
+	for _, m := range movies.Items {
+		set[m.NcgID] = m
+	}
+	for _, m := range other.Items {
+		set[m.NcgID] = m
+	}
+
+	items := make([]Movie, 0, len(set))
+	for _, movie := range set {
+		items = append(items, movie)
+	}
+	return &Movies{
+		TotalCount: len(items),
+		Items:      items,
+	}
 }
 
 // Movies returns detailed information about the given NCG IDs.
@@ -186,8 +211,8 @@ func (client *Client) UpcomingMovies(ctx context.Context, cityAlias string) (*Mo
 
 	if movies.TotalNbrOfItems == 0 || len(movies.Items) == 0 {
 		return &Movies{
-			TotalNbrOfItems: 0,
-			Items:           nil,
+			TotalCount: 0,
+			Items:      nil,
 		}, nil
 	}
 	ids := make([]string, len(movies.Items))
--- filmstaden/get_movies_test.go
+++ filmstaden/get_movies_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -28,8 +29,8 @@ func TestClient_Movies(t *testing.T) {
 	client := filmstaden.NewClient(srv.URL, NoopCache())
 	movies, err := client.Movies(context.Background(), []string{"apa", "bepa"})
 	assert.NilError(t, err)
-	assert.Equal(t, movies.TotalNbrOfItems, 33) // matches how many are actually in the movies.json file
-	assert.Check(t, cmp.Len(movies.Items, movies.TotalNbrOfItems))
+	assert.Equal(t, movies.TotalCount, 33) // matches how many are actually in the movies.json file
+	assert.Check(t, cmp.Len(movies.Items, movies.TotalCount))
 	m := movies.Items[0]
 	assert.Equal(t, m.Title, "Spider-Man: No Way Home")
 	assert.Equal(t, m.OriginalTitle, "Spider-Man: No Way Home")
@@ -68,12 +69,127 @@ func TestClient_UpcomingMovies(t *testing.T) {
 	client := filmstaden.NewClient(srv.URL, NoopCache())
 	movies, err := client.UpcomingMovies(context.Background(), "GB")
 	assert.NilError(t, err)
-	assert.Equal(t, movies.TotalNbrOfItems, 78) // matches how many are actually in the movies-upcoming.json file
-	assert.Check(t, cmp.Len(movies.Items, movies.TotalNbrOfItems))
+	assert.Equal(t, movies.TotalCount, 78) // matches how many are actually in the movies-upcoming.json file
+	assert.Check(t, cmp.Len(movies.Items, movies.TotalCount))
 	m := movies.Items[0]
 	assert.Equal(t, m.Title, "Spegeln")
 	assert.Equal(t, m.OriginalTitle, "Zerkalo")
 	assert.Equal(t, m.ProductionYear, 1975)
 	assert.Equal(t, m.ReleaseDate, "1979-11-12T00:00:00")
 	assert.Equal(t, m.ReReleaseDate, "2022-01-05T11:00:00")
+}
+
+func TestMovies_Merge(t *testing.T) {
+	type fields struct {
+		TotalCount int
+		Items      []filmstaden.Movie
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		other  *filmstaden.Movies
+		want   *filmstaden.Movies
+	}{
+		{
+			name: "No items",
+			fields: fields{
+				TotalCount: 0,
+				Items:      nil,
+			},
+			other: nil,
+			want: &filmstaden.Movies{
+				TotalCount: 0,
+				Items:      nil,
+			},
+		},
+		{
+			name: "No merge, single",
+			fields: fields{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+				},
+			},
+			other: nil,
+			want: &filmstaden.Movies{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+				},
+			},
+		},
+		{
+			name: "Merge",
+			fields: fields{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+				},
+			},
+			other: &filmstaden.Movies{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "bepa"},
+				},
+			},
+			want: &filmstaden.Movies{
+				TotalCount: 2,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+					{NcgID: "bepa"},
+				},
+			},
+		},
+		{
+			name: "Merge with empty",
+			fields: fields{
+				TotalCount: 0,
+				Items:      nil,
+			},
+			other: &filmstaden.Movies{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "bepa"},
+				},
+			},
+			want: &filmstaden.Movies{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "bepa"},
+				},
+			},
+		},
+		{
+			name: "Merge with same",
+			fields: fields{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+				},
+			},
+			other: &filmstaden.Movies{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+				},
+			},
+			want: &filmstaden.Movies{
+				TotalCount: 1,
+				Items: []filmstaden.Movie{
+					{NcgID: "apa"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			movies := &filmstaden.Movies{
+				TotalCount: tt.fields.TotalCount,
+				Items:      tt.fields.Items,
+			}
+			if got := movies.Merge(tt.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Merge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
