9cb541cc4c3d92f5a3b4e858c7e093108df48452
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-01-03 11:01:19 +0100

    chore: add prettier after type gen

--- web/src/__generated__/types.ts
+++ web/src/__generated__/types.ts
@@ -1,8 +1,14 @@
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -19,33 +25,33 @@ export type Scalars = {
 };
 
 export type Commandments = {
-  __typename?: 'Commandments';
-  number: Scalars['Int'];
-  phrase: Scalars['String'];
+  __typename?: "Commandments";
+  number: Scalars["Int"];
+  phrase: Scalars["String"];
 };
 
 export type GiftCertificate = {
-  __typename?: 'GiftCertificate';
-  expireTime: Scalars['Time'];
-  number: Scalars['String'];
+  __typename?: "GiftCertificate";
+  expireTime: Scalars["Time"];
+  number: Scalars["String"];
   status: GiftCertificate_Status;
 };
 
 export type GiftCertificateInput = {
-  expireTime?: InputMaybe<Scalars['Time']>;
-  number: Scalars['String'];
+  expireTime?: InputMaybe<Scalars["Time"]>;
+  number: Scalars["String"];
 };
 
 export enum GiftCertificate_Status {
-  Available = 'AVAILABLE',
-  Expired = 'EXPIRED',
-  Pending = 'PENDING',
-  Unknown = 'UNKNOWN',
-  Used = 'USED'
+  Available = "AVAILABLE",
+  Expired = "EXPIRED",
+  Pending = "PENDING",
+  Unknown = "UNKNOWN",
+  Used = "USED",
 }
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   addGiftCertificates: User;
   deleteGiftCertificate: User;
   disableCalendarFeed: User;
@@ -53,51 +59,48 @@ export type Mutation = {
   updateUser: User;
 };
 
-
 export type MutationAddGiftCertificatesArgs = {
   giftCerts?: InputMaybe<Array<GiftCertificateInput>>;
 };
 
-
 export type MutationDeleteGiftCertificateArgs = {
   giftCert: GiftCertificateInput;
 };
-
 
 export type MutationUpdateUserArgs = {
   newInfo: UserDetailsInput;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   allCommandments: Array<Commandments>;
   currentUser: User;
   randomCommandment: Commandments;
 };
 
 export type User = {
-  __typename?: 'User';
-  avatarURL?: Maybe<Scalars['String']>;
-  calendarFeedId?: Maybe<Scalars['String']>;
-  calendarFeedUrl?: Maybe<Scalars['String']>;
-  email: Scalars['String'];
-  filmstadenMembershipId?: Maybe<Scalars['FilmstadenMembershipID']>;
-  firstName: Scalars['String'];
+  __typename?: "User";
+  avatarURL?: Maybe<Scalars["String"]>;
+  calendarFeedId?: Maybe<Scalars["String"]>;
+  calendarFeedUrl?: Maybe<Scalars["String"]>;
+  email: Scalars["String"];
+  filmstadenMembershipId?: Maybe<Scalars["FilmstadenMembershipID"]>;
+  firstName: Scalars["String"];
   giftCertificates: Array<GiftCertificate>;
-  id: Scalars['UUID'];
-  lastLoginTime: Scalars['Time'];
-  lastName: Scalars['String'];
-  name: Scalars['String'];
-  nick?: Maybe<Scalars['String']>;
-  phone?: Maybe<Scalars['String']>;
-  signupTime: Scalars['Time'];
-  updateTime: Scalars['Time'];
+  id: Scalars["UUID"];
+  lastLoginTime: Scalars["Time"];
+  lastName: Scalars["String"];
+  name: Scalars["String"];
+  nick?: Maybe<Scalars["String"]>;
+  phone?: Maybe<Scalars["String"]>;
+  signupTime: Scalars["Time"];
+  updateTime: Scalars["Time"];
 };
 
 export type UserDetailsInput = {
-  filmstadenMembershipId?: InputMaybe<Scalars['FilmstadenMembershipID']>;
-  firstName?: InputMaybe<Scalars['String']>;
-  lastName?: InputMaybe<Scalars['String']>;
-  nick?: InputMaybe<Scalars['String']>;
-  phone?: InputMaybe<Scalars['String']>;
+  filmstadenMembershipId?: InputMaybe<Scalars["FilmstadenMembershipID"]>;
+  firstName?: InputMaybe<Scalars["String"]>;
+  lastName?: InputMaybe<Scalars["String"]>;
+  nick?: InputMaybe<Scalars["String"]>;
+  phone?: InputMaybe<Scalars["String"]>;
 };
