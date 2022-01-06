fdd8645d28a8424f455e06c705c994076134d79f
Emil Edholm <bobby@verypublic.org>
2022-01-06 20:58:52 +0100

    chore(cache): increase Filmstaden cache to 12h

--- filmstaden/client.go
+++ filmstaden/client.go
@@ -16,7 +16,7 @@ const (
 	APIURL                 = "https://www.filmstaden.se/api/v2"
 	requestTimeout         = 10 * time.Second
 	defaultUserAgent       = "Mozilla/5.0 (Linux; Android 11; Pixel 3 XL Build/RQ1A.210205.004; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36"
-	defaultCacheExpiration = 60 * time.Minute
+	defaultCacheExpiration = 12 * time.Hour
 )
 
 type Client struct {
