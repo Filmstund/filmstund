af806949b2138e7aecce66dcdb3bad8dbf405538
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-01-09 17:04:34 +0100

    fix: infinite toggle payment spinner

--- web/src/use-cases/single-showing/AdminModalAfterTicketsBought.tsx
+++ web/src/use-cases/single-showing/AdminModalAfterTicketsBought.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import StatusMessageBox from "../common/utils/StatusMessageBox";
 import { PaymentParticipantsList } from "./components/PaymentParticipantsList";
-import {
-  ShowingAdminFragment,
-  useTogglePaidChangeMutation,
-} from "../../__generated__/types";
+import { useTogglePaidChangeMutation } from "../../__generated__/types";
 import { InputSpinner } from "./InputSpinner";
 import { AdminAttendeePaymentDetails } from "./containers/types";
+import { useToaster } from "../../common/toast/ToastContext";
 
 interface AdminModalAfterTicketsBoughtProps {
   showingId: string;
@@ -17,29 +15,37 @@ interface AdminModalAfterTicketsBoughtProps {
 export const AdminModalAfterTicketsBought: React.FC<
   AdminModalAfterTicketsBoughtProps
 > = ({ showingId, participantPaymentInfos }) => {
-  const [
-    { fetching: mutationInProgress, error: mutationError },
-    togglePaidChange,
-  ] = useTogglePaidChangeMutation();
+  const [isMutating, setIsMutating] = useState(false);
+  const [{ error: mutationError }, togglePaidChange] =
+    useTogglePaidChangeMutation();
+
+  const toast = useToaster();
+  const toastaa = useToaster();
 
   const handlePaidChange = useCallback(
-    (
-      info: NonNullable<
-        ShowingAdminFragment["adminPaymentDetails"]
-      >["attendees"][0]
-    ) => {
-      const { user, hasPaid, amountOwed } = info!;
+    async (info: AdminAttendeePaymentDetails) => {
+      const { user, hasPaid, amountOwed } = info;
 
-      togglePaidChange({
-        paymentInfo: {
-          amountOwed,
-          hasPaid,
-          showingID: showingId,
-          userID: user.id,
-        },
-      });
+      try {
+        setIsMutating(true);
+
+        const { error } = await togglePaidChange({
+          paymentInfo: {
+            amountOwed,
+            hasPaid,
+            showingID: showingId,
+            userID: user.id,
+          },
+        });
+
+        if (error) {
+          toast({ variant: "danger", text: error.message });
+        }
+      } finally {
+        setIsMutating(false);
+      }
     },
-    [showingId, togglePaidChange]
+    [toast, showingId, togglePaidChange]
   );
 
   return (
@@ -48,7 +54,7 @@ export const AdminModalAfterTicketsBought: React.FC<
         handlePaidChange={handlePaidChange}
         participants={participantPaymentInfos}
       />
-      {mutationInProgress && <InputSpinner />}
+      {isMutating && <InputSpinner />}
       <StatusMessageBox errors={mutationError ? [mutationError] : null} />
     </>
   );
