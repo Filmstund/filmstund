bc020b71cb4641a7390d1213463a7bbdf4c3a31d
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-02-02 21:39:44 +0100

    refactor: cleanup unused or unnecessary code

--- web/src/use-cases/common/utils/useStateWithHandleChange.ts
+++ web/src/use-cases/common/utils/useStateWithHandleChange.ts
@@ -3,7 +3,11 @@ import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 export const useStateWithHandleChange = <T>(
   initialValue: T,
   transformer?: (x: string) => T
-): [T, (event: ChangeEvent<any>) => void, Dispatch<SetStateAction<T>>] => {
+): [
+  T,
+  (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void,
+  Dispatch<SetStateAction<T>>
+] => {
   const [value, setValue] = useState<T>(initialValue);
   const handleChange = (event: ChangeEvent<any>) => {
     setValue(
